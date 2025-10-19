9675603aa7b6ebf92a509ba2a62cc5477a66e7f1
Ray-M05 <rachelmojena@gmail.com>
2025-10-19 17:40:46 -0400

    Add(Question-Bank): QuestionType and Bridges models

--- app/domains/question-bank/models/Subject.ts
+++ app/domains/question-bank/models/Subject.ts
@@ -1,6 +1,7 @@
 import { Model, STRING } from "sequelize";
 import { sequelize } from "../../../database/database";
-import Question from "./Question";
+import Topic from "./Topic";
+import SubjectTopic from "./SubjectTopic";
 
 class Subject extends Model {
   public id!: string;
@@ -11,10 +12,19 @@ Subject.init(
   {
     id: { type: STRING, primaryKey: true },
     name: { type: STRING(200), allowNull: false, unique: true },
+    program: { type: STRING(200), allowNull: false },
   },
-  { sequelize, tableName: "subjects" }
+  { sequelize, tableName: "Subjects" }
+
+  
 );
 
-Subject.hasMany(Question, { foreignKey: "subjectId", as: "questions" });
+Subject.belongsToMany(Topic, {
+  through: SubjectTopic,
+  as: "topics",
+  foreignKey: "subjectId",
+  otherKey: "topicId",
+});
+
 
 export default Subject;
