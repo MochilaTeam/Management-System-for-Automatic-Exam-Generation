e9600ca2552d548b7349bf9c597379502ba63ef3
perezcam <camiloperezhf@gmail.com>
2025-11-02 00:29:04 -0400

    feat/routes

---
+++ app/domains/user/api/routes/user.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import {updateUser,deleteUser, getUserById, createUser,listUsers } from "../controllers/user";
+
+const router = Router();
+
+router.get("/users",listUsers);
+router.get("/users/:userId", getUserById);
+router.post("/users", createUser);
+router.patch("/users/:userId", updateUser);
+router.delete("/users/:userId", deleteUser);
+
+export default router;
